148eab317c43a5b2a3cfaf5ba6e41bdbe88ac7a1
stuartj073 <stuartj073@gmail.com>
2022-05-27 15:22:38 +0000

    Routing requests

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
