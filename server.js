fd575e3fa2805e290d2c6352878e02054fdaef7d
stuartj073 <stuartj073@gmail.com>
2022-05-27 15:14:50 +0000

    Request and response headers

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('</html>');
+    res.end();
 });
 
 http.listen();
