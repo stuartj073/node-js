5c651efaa2300520617530c08f3eaf10980ed5bd
stuartj073 <stuartj073@gmail.com>
2022-05-30 16:18:53 +0000

    Modules system

--- practice.js
+++ practice.js
@@ -17,7 +17,30 @@ const server = http.createServer((req, res) => {
         res.setHeader('Location', '/');
         return res.end();
     }
-
 });
 
 server.listen(3000);
+
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {        
+        setTimeout(() => {
+            resolve('Done');
+        }, 1500);
+    });
+    return promise;
+};
+
+
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    })
+    .then(text2 => {
+        console.log(text);
+    })
+}, 2000);
+
+console.log('Hello');
--- server.js
+++ server.js
@@ -1,34 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
-        // MAKE SURE RETURN IS HERE
-        // OR ELSE THERE WILL BE
-        // AN ERROR
-    }
+const routes = require('./routes');
 
-    if(url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
-        // res.writeHead(302, {});
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText);
 
-http.listen();
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
