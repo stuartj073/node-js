577f71b638d4edca6259e805f482deb4d30a878c
stuartj073 <stuartj073@gmail.com>
2022-06-01 10:16:18 +0000

    Install express.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+const express = require('express');
+
 const routes = require('./routes');
 
 console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+// const server = http.createServer(routes.handler);
 
 server.listen(3000);
